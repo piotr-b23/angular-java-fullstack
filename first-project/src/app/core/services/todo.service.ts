b2d3ceda05e585a2151b1e33133fc3bb4d524253
Piotr B <gppiotrekgp23@gmail.com>
2025-05-27 20:19:03 +0200

    subject added

--- first-project/src/app/core/services/todo.service.ts
+++ first-project/src/app/core/services/todo.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../../shared/interfaces/todo.interface';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   public _todos: Todo[] = [];
+
+  public todoChanged = new Subject<Todo[]>();
 
   public get todos() {
     return this._todos.slice();
@@ -15,6 +18,7 @@ export class TodoService {
     const localStorageTodos = localStorage.getItem('todos');
     if (localStorageTodos) {
       this._todos = JSON.parse(localStorageTodos);
+      this.todoChanged.next(this._todos);
     }
   }
 
@@ -25,11 +29,13 @@ export class TodoService {
     };
     this._todos.push(newTodo);
     this.saveToLocalStorage();
+    this.todoChanged.next(this._todos);
   }
 
   deleteTodo(i: number) {
     this._todos = this.todos.filter((_todo, index) => index !== i);
     this.saveToLocalStorage();
+    this.todoChanged.next(this._todos);
   }
 
   changeTodoStatus(index: number) {
@@ -38,6 +44,7 @@ export class TodoService {
       isCompleted: !this.todos[index].isCompleted,
     };
     this.saveToLocalStorage();
+    this.todoChanged.next(this._todos);
   }
 
   public saveToLocalStorage() {
--- first-project/src/app/todo-list/todo-list.component.ts
+++ first-project/src/app/todo-list/todo-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Todo } from '../shared/interfaces/todo.interface';
 import { AlertComponent } from '../shared/components/alert/alert.component';
 import { AddTodoFormComponent } from './add-todo-form/add-todo-form.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoService } from '../core/services/todo.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,14 +12,24 @@ import { TodoService } from '../core/services/todo.service';
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
   public todos: Todo[] = [];
   public errorMsg: string = '';
+  sub: Subscription = new Subscription();
 
   constructor(private todoSerivce: TodoService) {}
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
   ngOnInit(): void {
     this.todos = this.todoSerivce.todos;
+    this.sub.add(
+      this.todoSerivce.todoChanged.subscribe({
+        next: (value) => (this.todos = value),
+      })
+    );
   }
 
   public addTodo(todo: string): void {
@@ -27,7 +38,6 @@ export class TodoListComponent implements OnInit {
       return;
     }
     this.todoSerivce.addTodo(todo);
-    this.todos = this.todoSerivce.todos;
   }
 
   public clearErrorMsg() {
@@ -36,11 +46,9 @@ export class TodoListComponent implements OnInit {
 
   deleteTodo(i: number) {
     this.todoSerivce.deleteTodo(i);
-    this.todos = this.todoSerivce.todos;
   }
 
   changeTodoStatus(index: number) {
     this.todoSerivce.changeTodoStatus(index);
-    this.todos = this.todoSerivce.todos;
   }
 }
